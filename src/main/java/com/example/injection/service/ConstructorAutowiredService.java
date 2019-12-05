270cbe7a0d0c45239d2e6a17f94fa84989439ec2
nouzui2007 <nouzui2007@gmail.com>
2019-12-05 19:31:55 +0900

    add constructor injection sample

---
+++ src/main/java/com/example/injection/service/ConstructorAutowiredService.java
@@ -0,0 +1,30 @@
+package com.example.injection.service;
+
+import javax.annotation.PostConstruct;
+
+import com.example.injection.component.IComponent;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * ConstructorAutowiredService
+ */
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class ConstructorAutowiredService {
+
+    // @RequiredArgsConstructorを使った場合は@Autowiredは省略できる
+    private final IComponent firstComponent;
+
+    // もちろんConstructorも省略できる
+
+    @PostConstruct
+    public void execute() {
+        log.info("execute()");
+        this.firstComponent.execute();
+    }
+}
